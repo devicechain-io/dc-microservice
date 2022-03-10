24727a751547f5cb84f048aa113fcf837e9a21a7
Derek Adams <dadams@figure.com>
2022-03-10 11:17:38 -0500

    adding instance configuration

--- main.go
+++ main.go
@@ -12,12 +12,15 @@ import (
 	"time"
 
 	"github.com/devicechain-io/dc-microservice/cmd"
+	"github.com/devicechain-io/dc-microservice/core"
+	"github.com/devicechain-io/dc-microservice/generator"
 	"github.com/fatih/color"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 )
 
 func main() {
+	testGenerate()
 	banner()
 	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr, TimeFormat: time.RFC3339})
 	cmd.Execute()
@@ -33,4 +36,19 @@ func banner() {
 /_____/\___/|___/_/\___/\___/\____/_/ /_/\__,_/_/_/ /_/ 
 
 `))
+}
+
+type Stuff struct {
+	Name string
+	Last string
+}
+
+func testGenerate() {
+	config := core.NewDefaultInstanceConfiguration()
+
+	raw, err := generator.GenerateInstanceConfig("my-test-config", config)
+	if err != nil {
+		log.Error().Err(err).Stack().Msg("unable to create configuration")
+	}
+	fmt.Printf("yaml content:\n\n%s", string(raw))
 }
